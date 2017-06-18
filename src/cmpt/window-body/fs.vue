1282c4560b4f5a66121cbc8164b9d9f6eac2e6e5
hezedu@hotmail.com <hezedu@hotmail.com>
2017-06-18 20:32:28 +0800

    fs keydown

--- src/cmpt/window-body/fs.vue
+++ src/cmpt/window-body/fs.vue
@@ -1,13 +1,16 @@
 <template lang="jade">
 .lr-window-body
     .lr-fs-bar
-      .lr-3-item.glyphicon.glyphicon-arrow-left
-      .lr-3-item.glyphicon.glyphicon-arrow-up
-      .lr-3-item.glyphicon.glyphicon-arrow-right
-      input.lr-2-address(v-model='address')
-      .lr-3-item.glyphicon.glyphicon-play
-    .lr-fs-body
-      .lr-file(v-for='item in data', :class='{lr_2_file_link: item.isSymbolicLink}')
+      .lr-3-item.glyphicon.glyphicon-arrow-left(@click='handleArrowLeftClick', :class='{lr_2_disabled:backStack.length === 0}')
+      .lr-3-item.glyphicon.glyphicon-arrow-up(@click='handleArrowUpClick', :class='{lr_2_disabled:address === "/"}')
+      .lr-3-item.glyphicon.glyphicon-arrow-right(@click='handleArrowRightClick', :class='{lr_2_disabled:goStack.length === 0}')
+      input.lr-2-address(v-model='inputAddress' @keydown.13='handleGoClick')
+      .lr-3-item.glyphicon.glyphicon-repeat(v-if='address===inputAddress' @click='getData')
+      .lr-3-item.glyphicon.glyphicon-play(v-else @click='handleGoClick')
+    .lr-fs-body(v-if='error')
+      h2(v-html='data' style='color:red')
+    .lr-fs-body(v-else)
+      .lr-file(v-for='item in data', @click='handleItemClick(item)', :class='{lr_2_file_link: item.isSymbolicLink}')
         .lr-2-icon.glyphicon.glyphicon-file(v-if='!item.isDirectory')
         .lr-2-icon.glyphicon.glyphicon-folder-close(v-else-if='item.isDirectory')
         .lr-2-name {{item.name}}
@@ -17,28 +20,95 @@
 import store from '__ROOT__/store-global';
 export default {
   data(){
-    console.log(store.state)
     const pData = this.$parent;
-    const address = pData.address || store.state.homedir;
+    // const address = pData.address || store.state.homedir;
     return {
       isRequest: false,
-      address,
-      data: []
+      inputAddress: null,
+      // address: null,
+      addressArr: ['/'],
+      data: [],
+      backStack: [],
+      goStack: []
+    }
+  },
+  watch: {
+    address(v){
+      this.inputAddress = v;
+      this.getData();
+    }
+  },
+  computed: {
+    error(){
+      return !Array.isArray(this.data);
+    },
+    address(){
+      return '/' + this.addressArr.join('/');
     }
   },
   methods: {
+    setAddress(path){
+      const arr = path.split('/');
+      const arr2 = [];
+      arr.forEach(v => {
+        if(v){
+          arr2.push(v);
+        }
+      });
+      this.addressArr = arr2;
+      //this.address = '/' + arr2.join('/');
+    },
+    // setAddressByArr(){
+    //   this.address = '/' + this.addressArr.join('/');
+    // },
+    handleArrowLeftClick(){
+      if(!this.backStack.length) return;
+      this.goStack.push(this.addressArr);
+      const pop = this.backStack.pop();
+      this.addressArr = pop;
+    },
+    handleArrowRightClick(){
+      if(!this.goStack.length) return;
+      const pop = this.goStack.pop();
+      this.addressArr = pop;
+    },
+    handleGoClick(){
+      //console.log('handleGoClick')
+      this.setAddress(this.inputAddress);
+    },
+    handleArrowUpClick(){
+      if(!this.address === '/') return;
+      this.goStack = [];
+      this.backStack.push(this.addressArr);
+      const i = this.address.lastIndexOf('/');
+      this.setAddress(this.address.substr(0, i));
+    },
+    handleItemClick(item){
+      if(item.isDirectory){
+        this.backStack.push(this.addressArr);
+        this.setAddress(this.address + '/' + item.name);
+
+        //this.address = this.address + '/' + item.name;
+      }
+    },
     getData(){
       this.request({
         url: '~/fs' + this.address,
+        //poolKey: this.address,
         data: {dir: true},
         success(data){
           this.data = data;
+        },
+        error(xhr){
+          this.data = `http ${xhr.status} 错误: <br>${xhr.responseText}`
         }
       })
     }
   },
   created(){
-    this.getData();
+    const pData = this.$parent;
+    const address = pData.address || store.state.homedir;
+    this.setAddress(address);
   }
 }
 </script>
