29a9531409464f221f0ffa9b4b0ffcaa52f633f1
hezedu@hotmail.com <hezedu@hotmail.com>
2017-06-23 23:13:02 +0800

    closeAll

--- src/cmpt/tasks-bar.vue
+++ src/cmpt/tasks-bar.vue
@@ -1,11 +1,16 @@
 <template lang="jade">
 .lr-task-bar
-  .lr-task-item(v-for='i in tasks', @click.stop= 'handleClick(i)', :class='{lr_focus: i.focus, lr_close: i.isMin}')
-    {{i.name}} {{i.focus}}
+  .lr-2-body
+    .lr-task-item(v-for='i in tasks', @click.stop= 'handleClick(i)',
+    @contextmenu.prevent.stop = 'contextmenu(i, $event)',
+     :class='{lr_focus: i.focus, lr_close: i.isMin}')
+      {{i.name}} {{i.focus}}
+  .lr-2-control(title='Close All', @click='closeAll') X
 </template>
 
 <script>
 import store from '__ROOT__/store-global';
+import contextmenuStore from '__ROOT__/store/contextmenu';
 export default {
   data(){
     return {
@@ -13,6 +18,23 @@ export default {
     }
   },
   methods: {
+    closeAll(){
+      store.commit('set', {tasks: []});
+    },
+    contextmenu(task, e){
+      contextmenuStore.commit('open', {
+        data: [
+          {
+            name: 'Close',
+            handleClick(){
+              store.commit('removeTask', task);
+            }
+          }
+        ],
+        top: e.clientY - 32,
+        left: e.clientX
+      });
+    },
     handleClick(task){
       if(!task.isMin){
         store.commit('hiddenTask', task);
