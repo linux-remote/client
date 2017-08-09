6c85d4916467f5d635dc6b9ee2ead0d501caaaff
hezedu@hotmail.com <hezedu@hotmail.com>
2017-08-09 20:42:58 +0800

    lr_per_i

--- src/cmpt/window-body/fs-icon.vue
+++ src/cmpt/window-body/fs-icon.vue
@@ -3,7 +3,7 @@
   .lr-file-icon-front
   .lr-per-relation-wrap
     .lr-per-relation(v-for='(v, k) in item.rwxs',
-     :class='{lr_per_is: item["is_" + k], lr_per_i: item._relation === k}')
+     :class='{lr_per_is: item["is_" + k]}')
       .lr-per-item(v-for='v2 in v' , :class='{lr_per_item_on: v2 }')
     .lr-per-sticky(v-if='item.isSticky')
   span(v-if='item.suffix') {{item.suffix}}
