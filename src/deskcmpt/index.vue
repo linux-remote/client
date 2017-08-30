2822c1f2a09ff7bf79128e686eba12889babc97e
duwei <tianxiazhizhong@gmail.com>
2017-08-30 19:12:53 +0800

    OK

--- src/deskcmpt/index.vue
+++ src/deskcmpt/index.vue
@@ -1,9 +1,33 @@
-<template lang="jade">
-h1 deskcmpt
+<style>
+.demo{
+  position: absolute;
+  width: 200px;height: 200px;
+  /*min-width: 150vw;*/
+  background-color: blue;
+}
+</style>
+<template lang='jade'>
+  .demo(:style='{width:resize.w + "px", height: resize.h  + "px",  top: resize.y + "px", left: resize.x  + "px"}')
+    Resizeble(:data='resize')
 </template>
 
 <script>
+import Resizeble from './resizable.vue';
 export default {
-
+  data(){
+    return {
+      resize: {
+        x: 30,
+        y: 30,
+        w: 300,
+        h: 300,
+        minH: 200,
+        minW: 200
+      }
+    }
+  },
+  components: {
+    Resizeble
+  }
 }
 </script>
