6de964b62841e194a2f96d1e2f9249c1415975e2
tianxiazhizhong <tianxiazhizhong@gmail.com>
2018-05-25 01:14:34 +0800

    开始

---
+++ src/cmpt/start.vue
@@ -0,0 +1,24 @@
+
+<style>
+.lr-start-btn{
+  width: 80px;
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url(/public/img/tango-style/start-here.png);
+  border: 1px outset #999;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+<template lang="jade">
+.lr-start-btn
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
