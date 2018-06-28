5c4f71a99865ebde0dc2d9da49a43ad06f2392d8
tianxiazhizhong <tianxiazhizhong@gmail.com>
2018-06-28 20:11:39 +0800

    bak

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = __dirname;
+const nodeModuleStatic = require('./node-module-static');
+const map = Object.create(null);
+['jquery', 'vue', 'vuex', 'vue-router'].forEach(name => {
+  map[name] = nodeModuleStatic(name)
+})
+
+exports.dir = __dirname;
+exports.moduleMap = map;
--- setup.js
+++ setup.js
@@ -2,27 +2,19 @@ const favicon = require('serve-favicon');
 const path = require('path');
 var eStatic = require('express').static;
 
-const nodeModuleStatic = require('./node-module-static');
-
 const DAY_TIME = 1000 * 60 * 60 * 24 //一天
 const MONTH_TIME  = DAY_TIME * 30 //一月
 const HALF_YEAR_TIME  = MONTH_TIME * 6; //半年
 const IS_PRO = process.env.NODE_ENV === 'production';
-const map = Object.create(null);
+const map = require('./index').moduleMap;
 var distJsPathArr = [];
 
-function _staticByName(name){
-  const m = nodeModuleStatic(name);
-  map[name] = m;
+for(let name in map){
   var distName = IS_PRO ? name + '.min' : name;
-  var jsPath = m.url + '/' + name + '.js';
-  //console.log('jsPath', jsPath, m)
+  var v = map[name];
+  var jsPath = v.url + '/' + distName + '.js';
   distJsPathArr.push(jsPath);
 }
-
-['jquery', 'vue', 'vuex', 'vue-router'].forEach(v => {
-  _staticByName(v);
-})
 
 function setup(app){
   app.use(favicon(path.join(__dirname,  'logo_def.png')));
@@ -31,7 +23,6 @@ function setup(app){
     app.use(v.url, eStatic(v.fsDir, {maxAge:HALF_YEAR_TIME}));
   }
 };
-
 
 setup.nodeModuleStatic = distJsPathArr;
 
