87eca36baa00a6bfe9fcf7fba6a4fe8f991b9a00
tianxiazhizhong <tianxiazhizhong@gmail.com>
2018-08-04 08:54:02 +0800

    webpack 3

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
-// webpack 1.13.2
-// https://github.com/webpack/webpack/issues/1315#issuecomment-247269598
 var path = require('path');
-//var rucksack = require('rucksack-css');
 var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 var confName = process.env.NODE_BUILD_CONF_NAME || 'dev';
 var conf = require('./config/' + confName);
 var package = require('./package.json');
@@ -11,14 +10,29 @@ var setup = require('./setup');
 var isPro = process.env.NODE_ENV === 'production';
 var bundleName = conf.bundleName;
 var chunkName = conf.chunkName;
-var vueUseVuexPath = require.resolve('vue-use-vuex');
-var outputPath, publicPath;
+
+var outputPath, publicPath, cssRule;
 if(confName === 'dev' && !isPro){ //使用 命令weblack
   outputPath = path.join(__dirname, conf.indexDir, '/dist/dev/build');
   publicPath = conf.baseUrl + '/dist/dev/build/';
+  cssRule = {
+    test: /\.scss$/,
+    use: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.join(__dirname, './src')
+  }
+
 }else{
   outputPath = path.join(__dirname, conf.indexDir, '/build');
   publicPath = conf.baseUrl + '/build/';
+  cssRule = {
+    test: /\.scss$/,
+    //use: ["css-loader", "postcss-loader", "sass-loader"]
+    use: ExtractTextPlugin.extract({
+      use: ["css-loader",  "sass-loader"],
+      fallback: "style-loader"
+    })
+  }
+
 }
 
 var indexData = conf.indexData || {};
@@ -27,23 +41,13 @@ indexData.VERSION = package.version;
 
 // ***************************** plugins *****************************
 var plugins = [
-  new webpack.optimize.DedupePlugin(),
-  //提取公共模块
-  // new webpack.optimize.CommonsChunkPlugin({
-  //   names: [
-  //         //顺序不要变，名字也不要变
-  //         'b_1_lib'
-  //         ],
-  //   filename: bundleName}),
 
-  // new webpack.ProvidePlugin({
-  //   Vue: "vue"
-  // }),
   new webpack.DefinePlugin({
     'process.env': {
       NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
     }
   }),
+
   // create index.html
   new HtmlWebpackPlugin({
     chunksSortMode: 'dependency',
@@ -53,10 +57,22 @@ var plugins = [
     indexData
   })
 ]
+var rules = [
+  {
+    test: /\.js$/,
+    loader: 'babel-loader',
+    exclude: /node_modules/
+  },
+  {
+    test: /\.vue$/,
+    loader: 'vue-loader'
+  },
+  cssRule
+]
 // ***************************** 环境适配 *****************************
-var preLoaders;
 if (isPro) {
-  plugins.push(//正式环境下压缩
+  plugins = plugins.concat([//正式环境下压缩
+    new ExtractTextPlugin("styles_[contenthash].css"),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false,
@@ -64,29 +80,25 @@ if (isPro) {
       sourceMap: false,
       output: {
         comments: false,
-      },
-    }));
-
-    preLoaders = [ //代码检查
-      {
-        test: /(\.js|\.vue)$/,
-        loader: 'eslint-loader',
-        include: [path.resolve(__dirname, "src")],
-        exclude: [/node_modules/]
       }
-  ]
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    }) 
+  ]);
+
+  rules.unshift({
+    enforce: "pre",
+    test: /(\.js|\.vue)$/,
+    include: [path.resolve(__dirname, "src")],
+    loader: "eslint-loader"
+  })
 };
 
 // ***************************** conf *****************************
 module.exports = {
   context: path.join(__dirname, './src'),
-  entry: { //使用开头字母排序，防止vendor随着app代玛改变而改变。hack this bug https://github.com/webpack/webpack/pull/2998
-    //b_1_lib: ['lodash'],
-    // b_2_vendor: [
-    //   'vue',
-    //   'vue-router',
-    //   'vuex'
-    // ],
+  entry: {
     z_app: "./app.js"
   },
   output: {
@@ -96,43 +108,22 @@ module.exports = {
     chunkFilename: chunkName
   },
   module: {
-    preLoaders,
-    loaders: [{
-      test: /\.js$/,
-      include: [path.join(__dirname, "src"), vueUseVuexPath],
-      loader: 'babel-loader'
-    },
-    {
-      test: /\.vue$/,
-      loader: 'vue-loader'
-    },
-    {test: /\.css$/, loaders: ['style', 'css']},
-    {
-      test: /\.scss$/,
-      loaders: [
-        "style",
-        "css",
-        //"postcss",
-        "sass"
-      ]
-    }]
+    rules
   },
   resolve: {
-    extensions: ['', '.js'],
+    extensions: ['.js'],
     alias: {
       '__ROOT__' : path.join(__dirname, './src')
     }
   },
-  // postcss: [
-  //   rucksack({
-  //     autoprefixer: true
-  //   })
-  // ],
   plugins: plugins,
   devServer: {
     setup,
     contentBase: path.join(__dirname, conf.indexDir),
     hot: true,
     noInfo: true
+  },
+  performance: {
+    hints: false
   }
 };
