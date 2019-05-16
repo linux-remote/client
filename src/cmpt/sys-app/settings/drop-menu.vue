e3f36b19ff065beb216d5e1cc47fb9088fc6a2e7
tianxiazhizhong <tianxiazhizhong@gmail.com>
2019-05-16 21:31:45 +0800

    clear

--- src/cmpt/sys-app/settings/drop-menu.vue
+++ src/cmpt/sys-app/settings/drop-menu.vue
@@ -1,3 +1,19 @@
+<style>
+.lr-drop-menu-wrap{
+  position: relative;
+}
+.lr-drop-menu{
+  position: absolute; 
+  top: 120%;
+  right: -5px;
+  width: 255px;
+  background-color: #fff;
+  border: 2px outset #eee;
+}
+.lr-drop-menu-btn .lr_drop_menu_active, .lr-drop-menu-btn:hover{
+  background-color: rgba(#fff, .5);
+}
+</style>
 <template lang="jade">
 .lr-drop-menu-wrap
   .lr-drop-menu-btn(@click.stop='show', :class='{lr_drop_menu_active:showDrop}')
