76b560be1ffd5dc33ab7a66b65f8cb60c15dbdbd
tianxiazhizhong <tianxiazhizhong@gmail.com>
2019-06-15 12:41:48 +0800

    up

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # linux-remote-client
 - Production build: `npm run pro`
 - Development build: `npm run build`
+# Release
+1. Update `package.json` version.
+2. `npm run pro`
+3. `npm pubish`
--- index.js
+++ index.js
@@ -4,13 +4,9 @@ const path = require('path');
 let publicPath = require('lr-public');
 if(process.env.NODE_ENV !== 'production') {
   publicPath = path.join(__dirname, '../lr-public/public');
-  // map['local_winstrap'] = getUrlAndDir('local_winstrap', path.join(__dirname, '../../winstrap'));
-  // map['local_winstrap'].files = ['css/winstrap-optional.min.css'];
   
 } else {
 
-  // map['@hezedu/winstrap'] = getUrlAndDir('@hezedu/winstrap');
-  // map['@hezedu/winstrap'].files = ['css/winstrap-optional.min.css'];
 }
 
 map['jquery'] = getUrlAndDir('jquery');
