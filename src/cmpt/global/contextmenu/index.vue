76f60949eb6863b0f1c6852f7226611e10b35fc0
tianxiazhizhong <tianxiazhizhong@gmail.com>
2019-07-13 10:15:57 +0800

    Finish: 右键菜单 beforeShow https://github.com/linux-remote/linux-remote/issues/201

--- src/cmpt/global/contextmenu/index.vue
+++ src/cmpt/global/contextmenu/index.vue
@@ -25,6 +25,7 @@ export default {
       this.isShow = false;
     },
     show(e) {
+      this.$emit('beforeShow');
       window.APP.contextMenuTransferData = {
         x: e.clientX,
         y: e.clientY
