14a754286408fa800bc28f75c477e7d629e50eb4
tianxiazhizhong <tianxiazhizhong@gmail.com>
2020-02-03 22:15:26 +0800

    mount open icon

--- setup.js
+++ setup.js
@@ -2,14 +2,14 @@ const path = require('path');
 
 const favicon = require('serve-favicon');
 const eStatic = require('express').static;
-const clientMount = require('../client-mount/index.js');
+const clientMount = require('../client-index/index.js');
 
 const faviconPath = path.join(__dirname,  'logo_def.png');
 
 function setup(app, webpackDevServer){
   
 
-  clientMount(app, {_dev: '/dist/lr-client.js', CORS: 'http://192.168.56.101:3000'});
+  clientMount(app, {_dev: '/dist/lr-client.js', eStatic, CORS: 'http://192.168.56.101:3000'});
 
   // setTimeout(function(){
   //   // server.listeningApp eq http server.
