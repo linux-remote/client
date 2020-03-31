fd284d938b549426ef608765891422d28dc1fb78
tianxiazhizhong <tianxiazhizhong@gmail.com>
2020-03-31 13:30:52 +0800

    open-with

---
+++ src/sys-app/fs/folder/open-with-modal.vue
@@ -0,0 +1,32 @@
+<template lang="jade">
+.lr-window_body
+  .lr-fs_ow_wrap
+    .lr-fs_ow-item(@click="handleItemClick")
+      .lr-icon(v-open-icon="app.iconUrl")
+      span {{app.name}}
+  //- .lr-hr
+  //- .lr-fs_ow_footer
+  //-   button.lr-btn(ref="btn", @click="handleItemClick") OK
+</template>
+
+<script>
+// false
+import {getApp} from '../../../store/module/sys-apps-map';
+export default {
+  props: ['ok'],
+  data(){
+    return {
+      app: getApp('sys_app_editor')
+    }
+  },
+  methods: {
+    handleItemClick(){
+      this.ok('sys_app_editor');
+      this.$parent.close();
+    }
+  },
+  // mounted(){
+  //   this.$parent.setEnterBindBtn(this.$refs.btn);
+  // }
+}
+</script>
