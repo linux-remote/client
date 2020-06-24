58489e1cf6178596004366b1c40d2510a75bb40d
hezedu <hezedu@hotmail.com>
2020-06-24 09:00:52 +0800

    LRPCA

--- src/lib/constant.js
+++ src/lib/constant.js
@@ -1,5 +1,13 @@
 
-export const API_PATH = '/__LR_P_C_A_P__';
+function _getPort(){
+  let cors = window.CLIENT_CONFIG.CORS;
+  if(!cors){
+    return location.port;
+  }
+  const i = cors.lastIndexOf(':');
+  return cors.substr(i + 1);
+}
+export const API_PATH = '/_LRPCA_' + _getPort();
 export const AFR_TIMEOUT = 15 * 60 * 1000;
 export const WS_RECONNECT_TIME = 15 * 60 * 1000;
 export const SR_KEY_TERM_WRITE = 2;
